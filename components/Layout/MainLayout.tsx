7877d4497fe2d527e305fd4a84bdc889f6e2a062
thanwannahtun <thanwannahtun.dev.lmp@gmail.com>
2026-03-03 10:35:26 +0630

    fix: width issues in Large screen [ css - min-h-screen w-full ]

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -32,13 +32,12 @@ export function MainLayout({ children }: MainLayoutProps) {
         {/* Main Content Area */}
         <div
           // className="flex-1 flex flex-col min-w-0 "
-          className="flex flex-col h-screen min-w-0 "
+          className="flex flex-col min-h-screen w-full "
         // className="flex-1 flex flex-col min-w-0 lg:pl-64"
         >
           {/* Note: lg:pl-64 must match your Sidebar's desktop width */}
           <Header onMenuClick={toggleSidebar} />
-
-          <main className="p-4 md:p-6 lg:p-8 max-w-[1600px] mx-auto w-full overflow-y-auto">
+          <main className="p-4 md:p-6 lg:p-8 mx-auto w-full overflow-y-auto">
             {children}
           </main>
         </div>
